dd4503979b7b1d29d712ac582e75c5ceef813a58
Anastasiya <anastasiya.ibr@mail.ru>
2025-03-29 17:54:40 +0300

    feat: добюзвук начала игры

--- src/components/pages/gamePage/index.tsx
+++ src/components/pages/gamePage/index.tsx
@@ -2,10 +2,17 @@ import { useLocation } from 'react-router-dom';
 import { cards } from '../../../models/constants';
 import { GameField } from '../../ui/gameField';
 import { shuffleAndEditSize } from '../../../utils/shuffleAndEditSize';
+import { useEffect } from 'react';
+import startSound from '../../../assets/sounds/begin.mp3';
 
 export const GamePage = () => {
   const location = useLocation();
   const { state } = location;
+  const beginSound = new Audio(startSound);
+
+  useEffect(() => {
+    beginSound.play();
+  }, []);
 
   return (
     <div>
